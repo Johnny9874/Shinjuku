14d6a599a96a71df24ebde1e2b91097acf4f1324
JohnnyYe <ye.johnnypro@gmail.com>
2025-11-06 01:05:59 +0100

    ajout photo 1st section into public and starting importing those photo into the HomePage and styling them

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,14 @@ export default function HomePage() {
   return (
       <main>
         <Navbar />
+        <section className="flex items-center min-h-screen bg-gray-100 pl-60">
+          <div className="text-center">
+            <img src="/logo_shinjuku.png" alt="Shinjuku Logo" className="h-30 md:h-100" />
+            <p>Where culinary artistry meets <br />ocean freshness, redefining sushi <br />excellence in every bite.</p>
+            <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors">Make a Reservation</button>
+          </div>
+        </section>
+        <section className="bg-black h-60"></section>
       </main>
   );
 }
